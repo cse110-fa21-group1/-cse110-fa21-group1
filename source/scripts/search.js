9341dbae735133bec3a67628e29bd00dea663645
darwinchan2002 <p3chan@ucsd.edu>
2021-11-17 02:08:11 -0800

    Added search from spoonacular

--- source/scripts/search.js
+++ source/scripts/search.js
@@ -1,17 +1,18 @@
 window.addEventListener('DOMContentLoaded', init);
 
-const apiKey = "e4948e03b38847d7b0b4c0e30e37bd17"
-const recipeData = []
+const apiKey = 'e4948e03b38847d7b0b4c0e30e37bd17';
+const recipeData = [];
 
 /** Populate recipe cards */
 async function init() {
-    const queryString = window.location.search;
-    const urlParams = new URLSearchParams(queryString);
-    await populateRecipeCards(urlParams.get('q'))
-    const recipeCard = document.querySelector(".recipe-cards--wrapper")
-    recipeCard.querySelector('img').setAttribute('src', recipeData[0]['image'])
-    recipeCard.querySelector('p').querySelector('a').innerText = recipeData[0]['title']
-    recipeCard.querySelector('p').querySelector('a').href = recipeData[0]['sourceUrl']
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  await populateRecipeCards(urlParams.get('q'));
+  const recipeCard = document.querySelector('.recipe-cards--wrapper');
+  recipeCard.querySelector('img').setAttribute('src', recipeData[0]['image']);
+  const title = recipeCard.querySelector('p').querySelector('a');
+  title.innerText = recipeData[0]['title'];
+  title.href = recipeData[0]['sourceUrl'];
 }
 
 /**
@@ -19,36 +20,37 @@ async function init() {
  * @param {String} query Query string for the search
  */
 async function populateRecipeCards(query) {
-    const queryURL = "https://api.spoonacular.com/recipes/complexSearch?query=" + query
-        + "&addRecipeInformation=true"
-        + "&number=10"
-        + "&apiKey=" + apiKey
-    try {
-        await fetchRecipes(queryURL)
-    } catch (err) {
-        console.log(`Error fetching recipes: ${err}`);
-    }
+  const queryURL = 'https://api.spoonacular.com/recipes/complexSearch?query=' + query +
+        '&addRecipeInformation=true' +
+        '&number=10' +
+        '&apiKey=' + apiKey;
+  try {
+    await fetchRecipes(queryURL);
+  } catch (err) {
+    console.log(`Error fetching recipes: ${err}`);
+  }
 }
 
 /**
  * Fetch recipes from the given url
  * @param {String} url url for fetching recipes
- * @returns Promise object for identifying job done
+ * @return {Promise} Promise object for identifying job done
  */
 async function fetchRecipes(url) {
-    return new Promise((resolve, reject) => {
-        fetch(url)
-            .then(response => response.json())
-            .then(data => {
-                for (ind in data['results']) {
-                    recipeData[recipeData.length] = data['results'][ind]
-                }
-                console.log(data['results'])
-                resolve()
-            })
-            .catch(err => {
-                console.log(`Error loading the ${url} recipe`);
-                reject(err);
-            });
-    });
+  return new Promise((resolve, reject) => {
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+          const length = data['results'].length;
+          for (let i = 0; i < length; i++) {
+            recipeData[recipeData.length] = data['results'][ind];
+          }
+          console.log(data['results']);
+          resolve();
+        })
+        .catch((err) => {
+          console.log(`Error loading the ${url} recipe`);
+          reject(err);
+        });
+  });
 }
