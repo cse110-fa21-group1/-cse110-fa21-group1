caec8af632d91961b864cc80a9f3fb129dcbceeb
darwinchan2002 <p3chan@ucsd.edu>
2021-11-17 23:09:44 -0800

    Added Recipe Demo

--- source/scripts/Recipe.js
+++ source/scripts/Recipe.js
@@ -18,7 +18,7 @@ export class Recipe {
   recipeInstruction;
   /* {String} Recipe video url */
   video;
-  /* {Int} Recipe ID */
+  /* {String} Recipe ID */
   id;
   /* {Array} Recipe tags */
   tags;
@@ -30,3 +30,18 @@ export class Recipe {
    */
   constructor() {}
 }
+
+export const recipeDemo = {};
+recipeDemo.name = 'The Demo Recipe';
+recipeDemo.image = 'https://www.simplyrecipes.com/thmb/OCi18J2V8OeKDFV3FxoeKvgq74E=/1423x1067/smart/filters:no_upscale()/__opt__aboutcom__coeus__resources__content_migration__simply_recipes__uploads__2012__07__grilled-sweet-potatoes-horiz-a-1600-7c8292daa98e4020b447f0dc97a45cb7.jpg';
+recipeDemo.datePublished = '2021-11-17';
+recipeDemo.description = 'The best recipe ever!';
+recipeDemo.totalTime = 'PT30M';
+recipeDemo.recipeYield = '100';
+recipeDemo.recipeIngredient = ['apple', 'banana', 'potatos'];
+recipeDemo.recipeInstruction = [
+  'Add apples.',
+  'Add bananas.',
+  'Add potatos. You are done!',
+];
+recipeDemo.id = '0000';
