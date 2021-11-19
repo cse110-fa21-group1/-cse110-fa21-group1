5f50b9fb9e5569ce58abf6eb9144f1821b1cfa1a
notelock <joy003@ucsd.edu>
2021-11-19 15:31:55 -0800

    yt lint

--- source/scripts/ManageRecipe.js
+++ source/scripts/ManageRecipe.js
@@ -81,8 +81,8 @@ box4.onclick = function() {
 function youtubeID(link) {
   const parsed = link.split(/(vi\/|v=|\/v\/|youtu\.be\/|\/embed\/)/);
    return (parsed[2] !== undefined) ? parsed[2].split(/[^0-9a-z_\-]/i)[0] : link;
-
 }
+
 /** Adds 'event listeners' for the import video
  *  and picture from URL buttons by setting src.
  *  */
