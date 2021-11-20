4d6f3d8bcb2dda83e9054e1cef6f1dcba847cb58
Tianze Guan <bill@Tianzes-MBP.lan>
2021-11-19 18:23:32 -0800

    add delete function

--- source/scripts/deleteRecipe.js
+++ source/scripts/deleteRecipe.js
@@ -1,5 +1,5 @@
 // deleteRecipe.js
-// When user click the delete button in at the top of the recipe, the 
+// When user click the delete button in at the top of the recipe, the
 // recipe should be removed and the user should be lead to the home page
 
 import {storage} from './storage.js';
@@ -7,15 +7,15 @@ import {storage} from './storage.js';
 const deleteButton = document.getElementById('delete-button');
 
 deleteButton.addEventListener('click', (event) => {
-  console.log('storage:' + storage);
+  // console.log('storage:' + storage);
   // Extract query id
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
   const id = urlParams.get('id');
-    
+
   // Remove the recipe
   storage.removeRecipe(id);
 
   // Go back to home page
-  window.location.href = 'http://127.0.0.1:5501/source/Home.html';
-})
+  window.location.href = window.location.pathname.replace('Recipe.html', 'Home.html');
+});
