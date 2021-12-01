7da2a104e2164ce58e648d991898cc0782dc5b1d
nabutabu <43729053+nabutabu@users.noreply.github.com>
2021-11-30 18:51:51 -0800

    Added lintignore file

--- source/.eslintrc.js
+++ source/.eslintrc.js
@@ -10,14 +10,6 @@ module.exports = {
     'ecmaVersion': 13,
     'sourceType': 'module',
   },
-  'overrides': [
-    {
-      'files': ['source/docs/*', 'source/docs/scripts/linenumber.js', 'scripts/prettify/lang-css.js', 'source/docs/scripts/prettify/prettify.js',], // Or *.test.js
-      'rules': {
-        'require-jsdoc': 'off',
-      },
-    },
-  ],
   'rules': {
   },
 };
